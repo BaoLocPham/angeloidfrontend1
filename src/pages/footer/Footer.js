5c0791976dac1b96b45baa63db45ba41c28a7117
Tran Thien Phu <hostcode0301@gmail.com>
2021-05-19 13:43:10 +0700

    phutt: footer created

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import logo from './image/Logo-FU-03.png';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+
+const style = {
+    backgroundColor: "#11161d",
+    height: "auto",
+    padding: "2rem",
+    color: "white"
+};
 
 const Footer = () => {
     return (
-        <footer>
-            I am Footer Page!
+        <footer className="row">
+            <div className="col-12 col-md-6" style={style}>
+                <div className="d-flex flex-row mb-4 mt-4">
+                    <i className="fa fa-map-marker fa-2x me-4" style={{color: "white"}}></i>
+                    <h5 className="text-end">FPT University</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                    <i className="fa fa-phone fa-2x me-3" style={{color: "white"}}></i>
+                    <h5 className="text-end">+84 0123 4567</h5>
+                </div>
+                <div className="d-flex flex-row">
+                    <i className="fa fa-envelope fa-2x me-2" style={{color: "white"}}></i>
+                    <h5 className="float-end">support_angloid@gmail.com</h5>
+                </div>
+            </div>
+            <div className="col-12 col-md-6" style={style}>
+                <h3>About the company</h3>
+                <p>We are web application making company. Our most important mission is bring the difference to our country. We here to make your dream be come true.</p>
+                <h3>Sponsor by</h3>
+                <img src={logo} alt="FPT logo"/>
+            </div>
         </footer>
     );
 }
