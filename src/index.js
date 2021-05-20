24e69819ebca89823d3c14145316796f0b9d2f9a
bao loc <phambaoloc163@gmail.com>
2021-05-20 23:34:49 +0700

    cha lam j ca

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import AnimeList from './pages/home/AnimeList';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AnimeList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
