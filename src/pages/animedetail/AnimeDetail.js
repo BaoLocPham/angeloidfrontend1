7d9ad8e2afff2f7a07a766d6163ca617f0862467
ViNguyenHoang <hoangvi0206@gmail.com>
2021-05-20 13:57:40 +0700

    Vi: change variable names

--- src/pages/animedetail/AnimeDetail.js
+++ src/pages/animedetail/AnimeDetail.js
@@ -7,31 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AnimeDetail = () => {
 
-    const cha = "https://s2.vndb.org/ch/87/62187.jpg";
-    const sei = "http://www.kenproduction.co.jp/admin/img/talent/131/1.jpg?r=502599940"
-
-    const frame = {
-        backgroundColor: "#19293B",
-        color: "white",
-        borderRadius: "8px",
-        fontSize: "0.8rem",
-    }
-
-    const chaImg = {
-        backgroundImage: `url(${cha})`,
-        backgroundSize: 'cover',
-        height: '7rem',
-        borderRadius: '10%',
-        backgroundPosition: "center",
-    }
-
-    const seiyuuImg = {
-        backgroundImage: `url(${sei})`,
-        backgroundSize: 'cover',
-        height: '7rem',
-        borderRadius: '10%',
-        backgroundPosition: "center",
-    }
     return (
         <>
             I am Anime Detail Page!
