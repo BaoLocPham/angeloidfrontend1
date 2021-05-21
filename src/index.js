3a6e7464a94637dabdb5a4f7e6260a832cd69489
bao loc <phambaoloc163@gmail.com>
2021-05-21 14:42:33 +0700

    modify: AnimeList AnimeCard AnimeTooltip libary: react-tooltip

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Home from './pages/home/Home'
 ReactDOM.render(
   <React.StrictMode>
-    <Home/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
