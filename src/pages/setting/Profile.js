657820d0a5a0a3d02d99c9345328fe73c76c70c9
bao loc <phambaoloc163@gmail.com>
2021-05-27 22:17:33 +0700

    BaoLoc:update avatar + Profile css

--- src/pages/setting/Profile.js
+++ src/pages/setting/Profile.js
@@ -5,10 +5,10 @@ import Avatar from './Avatar';
 const Profile = () => {
     return (
         <div className="row d-flex flex-column-reverse flex-md-row w-100 justify-content-center align-items-center">
-            <div className="col-8 p-5">
+            <div className="col-12 col-md-8 p-5">
                 <ProfileEdit />
             </div>
-            <div className="col-4 py-5">
+            <div className="col-12 col-md-4 py-5">
                 <Avatar />
             </div>
         </div>
