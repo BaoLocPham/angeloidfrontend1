ea285b06509a055ba4eca1ba534d49e7a3174bcd
bao loc <phambaoloc163@gmail.com>
2021-05-27 22:10:44 +0700

    BaoLoc: update avatar

--- src/pages/setting/Avatar.js
+++ src/pages/setting/Avatar.js
@@ -1,48 +1,50 @@
-import React,{useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
+import "./Avatar.css";
 const Avatar = () => {
-    
-    const wrapper={
-        backgroundColor:"#19293B", 
-        borderRadius:"5%",
-        color:"#fff",
-        margin:"2em 0.5em",
-        
+
+    const wrapper = {
+        backgroundColor: "#19293B",
+        borderRadius: "5%",
+        color: "#fff",
+        marginLeft:"1.3em",
+        textAlign: "center"
     }
-    const upper={
+    const upper = {
         borderTopLeftRadius: "5%",
         borderTopRightRadius: "5%",
         backgroundImage: `url("https://randomc.net/image/To%20LOVE-Ru/To%20LOVE-Ru%20-%20Trouble%20Darkness%20-%2001%20-%20Large%2022.jpg")`,
         backgroundSize: "cover",
-        backgroundPosition:"center"
+        backgroundPosition: "center",
+        padding:"7rem 0rem 9rem 0rem"
     }
-    const lower={
-        padding: "12em 0 1em 0",
+    const lower = {
+        padding: "8em 0 1em 0",
     }
-    const avatar={
-        width: "20em",
-        height: "20em",
-        borderRadius: "100%", 
-        margin: "10em 1em -10em 1em",   
+    const avatar = {
+        borderRadius: "100%",
+        backgroundImage:`url("https://i.pinimg.com/originals/92/a9/f6/92a9f6ec0d2eb46ac35417a2b4eddcdb.jpg")`,
+        backgroundSize:"cover",
+        backgroundPosition:"center",
+        margin: "2em auto -15em auto",
         cursor: "pointer",
+        border:"5px #19293B solid",
     }
     const textInput = useRef(null);
-    const showOpenFile=()=>{
+    const showOpenFile = () => {
         textInput.current.click();
     }
     return (
-            <div className="h-auto w-100" style={{ textAlign: "center"}}>
-                <div className="" style={wrapper}>
-                    <div style={upper}>
-                        <div>
-                        <input ref={textInput} type="file" accept="image/*" style={{display:"none"}}/>
-                        <img onClick={showOpenFile} style={avatar} src="https://i.pinimg.com/originals/92/a9/f6/92a9f6ec0d2eb46ac35417a2b4eddcdb.jpg" />
-                        </div>
-                    </div>
-                    <div sclassName="justify-content-center" style={lower} >
-                        <h3>BaoLoc</h3>
-                    </div>
+        <div className="h-auto" style={wrapper}>
+            <div style={upper}>
+                <div>
+                    <input ref={textInput} type="file" accept="image/*" style={{ display: "none" }} />
+                    <div className="avatar" onClick={showOpenFile} style={avatar}></div>
                 </div>
             </div>
+            <div className="justify-content-center" style={lower} >
+                <h3>BaoLoc</h3>
+            </div>
+        </div>
     );
 }
 
