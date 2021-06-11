b0025f7f7caf734de3fdd5f1bc87c0e613d165d4
Ngô Phúc <bapngo13401@gmail.com>
2021-06-11 22:21:31 +0700

    Corn: config for forgot password

--- src/pages/account/Account.js
+++ src/pages/account/Account.js
@@ -1,8 +1,9 @@
 // Import Libraries
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Switch,
-    Route
+    Route,
+    Redirect
 } from 'react-router-dom';
 
 // Import Components
@@ -22,14 +23,14 @@ import bg6 from './img/bg-06.jpg';
 import bg7 from './img/bg-07.jpg';
 import bg8 from './img/bg-08.jpg';
 
-const Account = ({ setUser,isLogin, setCookie }) => {
+const Account = ({ setUser, isLogin, setCookie }) => {
 
     //Random variable to get image random
     const [random, setRandom] = useState();
 
     //List of image, author and link to author page
     const bg = {
-        img : [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8],
+        img: [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8],
         author: ["DSマイル", "三年B组のsanaeさん", "U35(うみこ)", "banishment", "T5", "しぐれうい", "三湊かおり", "藤原"],
         link: ["795196", "3990881", "337971", "23223750", "4819066", "431873", "12742306", "27517"]
     }
@@ -48,34 +49,40 @@ const Account = ({ setUser,isLogin, setCookie }) => {
         setRandom((Math.floor(Math.random() * 7) + 1));
     }, [])
 
+    if (isLogin) {
+        return (
+            <Redirect to='/' />
+        );
+    }
     return (
         <Switch>
-                <Route path='/account/login'>
-                    <Login 
-                        setUser={setUser} 
-                        isLogin={isLogin}
-                        setCookie={setCookie}
-                        BackGround={BackGround}
-                        Author={Author}
-                        AuthorLink={AuthorLink}
-                    />
-                </Route>
+            <Route path='/account/login'>
+                <Login
+                    setUser={setUser}
+                    isLogin={isLogin}
+                    setCookie={setCookie}
+                    BackGround={BackGround}
+                    Author={Author}
+                    AuthorLink={AuthorLink}
+                />
+            </Route>
 
-                <Route path='/account/signup'>
-                    <Signup 
-                        BackGround = {BackGround}
-                        Author = {Author}
-                        AuthorLink = {AuthorLink}
-                    />
-                </Route>
+            <Route path='/account/signup'>
+                <Signup
+                    BackGround={BackGround}
+                    Author={Author}
+                    AuthorLink={AuthorLink}
+                />
+            </Route>
 
-                <Route path='/account/forgot'>
-                    <ForgotPassword />
-                </Route>
+            <Route path='/account/forgot'>
+                <ForgotPassword />
+            </Route>
 
-                <Route path='/account/change'>
-                    <ChangePassword />
-                </Route>
+            <Route path='/account/change'>
+                <ChangePassword />
+            </Route>
+            
             <Route path='*'>
                 <Error />
             </Route>
