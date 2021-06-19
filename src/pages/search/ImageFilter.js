29fcf41f550180e24dfa0a84c660dc1d2a4a9625
Trần Thiện Phú <hostcode0301@gmail.com>
2021-06-19 21:05:26 +0700

    phutt: image filter and drag drop file

--- src/pages/search/ImageFilter.js
+++ src/pages/search/ImageFilter.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ImageFilter = () => {
+const ImageFilter = ({ handleUploadImg}) => {
     return (
-        <div>
-            I am Image Filter!
+        <div className="image-filter d-flex flex-row">
+            <input class="form-control me-3" type="file" id="formFile" accept="image/png, image/jpeg" onChange={handleUploadImg}/>
+            {/* Change search type button */}
+            <div className="">
+                <Link to="/search">
+                    <button type="button" className="btn btn-success">
+                        <i className="fa fa-filter" aria-hidden="true"></i>
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
- 
+
 export default ImageFilter;
