bbab13dae1cbad912e8331c0f3087bf30dcdc57e
Ngô Phúc <bapngo13401@gmail.com>
2021-06-27 19:50:23 +0700

    Corn: Search function

--- src/adapters/SearchAPI.js
+++ src/adapters/SearchAPI.js
@@ -1,7 +1,20 @@
-const fetchTagList = async () => {
-    return await fetch(`${process.env.REACT_APP_BACKEND_URL}api/tag`).then(res => res.json());
-}
+const fetchSearchData = async (url) => {
+    var data = await fetch(url);
+    data = await data.json();
+    return data;
+};
 
-export {
-    fetchTagList
-}
+const search = async (url, body) => {
+    var data = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    data = await data.json();
+    return data;
+};
+
+export { fetchSearchData, search };
--- src/pages/search/SearchByText.js
+++ src/pages/search/SearchByText.js
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react';
+//#region Import
+// Libs
+import React, { useState, useEffect, useRef } from 'react';
+import { fetchSearchData, search } from '../../adapters/SearchAPI';
+// Components
 import TextFilter from './TextFilter';
 import SearchTagList from './SearchTagList';
 import SearchResult from './SearchResult';
+import Loading from '../components/Loading';
+import { data } from 'jquery';
+import Cell from './img/cells12d.jpg';
+import Kanna from '../error/Kanna.png';
+//#endregion Import
 
+//#region Const Declare
 const colorList = [
     { id: 1, colorCode: "#EC294B" },
     { id: 2, colorCode: "#F2AC57" },
@@ -13,45 +23,60 @@ const colorList = [
     { id: 7, colorCode: "#FF7171" },
 ]
 
+const quotes = {
+    1: { quote: "The two most powerful warriors are patience and time.", author: "Leo Tolstoy, War and Peace"},
+    2: { quote: "Somewhere, something incredible is waiting to be known.", author: "Sharon Begley"},
+    3: { quote: "You usually have to wait for that which is worth waiting for.", author: "Craig Bruce"},
+    4: { quote: "Life was always a matter of waiting for the right moment to act.", author: "Paulo Coelho"},
+    5: { quote: "Mars is there, waiting to be reached.", author: "Buzz Aldrin"},
+    6: { quote: "We're waiting for the pendulum to swing back again, which I am absolutely confident it will.", author: "Don Bluth"},
+    7: { quote: "Your heart is full of fertile seeds, waiting to sprout.", author: "Morihei Ueshiba"},
+    8: { quote: "We all have the extraordinary coded within us, waiting to be released.", author: "Jean Houston"},
+}
+
+const KANNA_IMG_STYLE = {
+    height: 360,
+    width: 360,
+    backgroundImage: `url(${Kanna})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover"
+}
+//#endregion Const Declare
+
 const SearchByText = () => {
-
+    // Check Loading
+    const [isLoading, setIsLoading] = useState('loading');
+    const [isAnimeLoading, setIsAnimeLoading] = useState('init');
     const [tags, setTags] = useState([]);
-
-    const years = [
-        { yearId: 1, year: "2021" },
-        { yearId: 2, year: "2019" },
-        { yearId: 3, year: "2018" },
-        { yearId: 4, year: "2017" },
-        { yearId: 5, year: "2016" }
-    ]
-
-    const seasons = [
-        { seasonId: 1, seasonName: "Spring" },
-        { seasonId: 2, seasonName: "Summer" },
-        { seasonId: 3, seasonName: "Autumn" },
-        { seasonId: 4, seasonName: "Winter" }
-    ]
+    const [years, setYears] = useState([]);
+    const seasons = ["Winter", "Spring", "Summer", "Fall"];
 
     const formats = [
-        { formatId: 1, formatName: "TV" },
-        { formatId: 2, formatName: "Movie" },
-        { formatId: 3, formatName: "Ova" },
-        { formatId: 4, formatName: "Special" }
+        { formatId: 1, formatName: "Oneshot/Movie" },
+        { formatId: 2, formatName: "Series" },
     ]
 
     useEffect(() => {
-        setTags()
+        Promise.all([
+            fetchSearchData(`${process.env.REACT_APP_BACKEND_URL}api/tag`),
+            fetchSearchData(`${process.env.REACT_APP_BACKEND_URL}api/season`),
+        ]).then(([tagList, seasonYearList]) => {
+            setTags(tagList);
+            setYears(seasonYearList.reverse());
+            setIsLoading('done');
+        });
     }, []);
 
     //State để quản lí các input form
     const [selectedName, setSelectedName] = useState("");
     const [selectedTag, setSelectedTag] = useState([]);
+    const [tagListToSend, setTagListToSend] = useState([]);
     const [selectedYear, setSelectedYear] = useState("");
     const [selectedSeason, setSelectedSeason] = useState("");
-    const [selectedFormat, setSelectedFormat] = useState("");
+    const [selectedFormat, setSelectedFormat] = useState(0);
 
     // Check các thay đổi trên các input 
-
     const handleSelectName = (event) => {
         setSelectedName(event.target.value);
     }
@@ -90,13 +115,15 @@ const SearchByText = () => {
     const handleSelectTag = (event) => {
         event.preventDefault();
         if (!containTagId(event.target.value)) {
-            setSelectedTag([...selectedTag, { tagId: event.target.value, tagName: getNameById(event.target.value), tagBgColor: setColor() }])
+            setSelectedTag([...selectedTag, { tagId: event.target.value, tagName: getNameById(event.target.value), tagBgColor: setColor() }]);
+            setTagListToSend([...tagListToSend, { tagId: event.target.value, tagName: getNameById(event.target.value) }]);
         }
     }
 
     // Handle Delete Selected Tag
     const handleDeleteTag = (tagId) => {
-        setSelectedTag(selectedTag.filter(tag => tag.tagId !== tagId))
+        setSelectedTag(selectedTag.filter(tag => tag.tagId !== tagId));
+        setTagListToSend(tagListToSend.filter(tag => tag.tagId !== tagId));
     }
 
     const handleSelectYear = (event) => {
@@ -112,28 +139,90 @@ const SearchByText = () => {
     }
 
     //Xuất dữ liệu ra console
+    const [searchResult, setSearchResult] = useState([]);
+    // Ref to disable btn when user submit data
+    let searchBtn = useRef(null);
+
+    const handleSubmitSearch = () => {
+        setIsAnimeLoading('loading');
+        // If Submit Button exist, disable it
+        // Prevent too many request to the server
+        if (searchBtn.current) {
+            searchBtn.current.setAttribute("disabled", "disabled");
+        }
+
+        var obj = {
+            animeName: selectedName,
+            year: selectedYear,
+            seasonName: selectedSeason,
+            tags: tagListToSend,
+            episode: selectedFormat.toString(), 
+        }
+
+        Promise.all([
+            search(`${process.env.REACT_APP_BACKEND_URL}api/search`, obj),
+        ]).then(([data]) => {
+            setSearchResult(data);
+            searchBtn.current.removeAttribute("disabled");
+        });
+    }
+
+    const getRandomQuote = () => {
+        return quotes[(Math.floor(Math.random() * 8) + 1)]
+    }
 
     useEffect(() => {
-        console.log("Name: " + selectedName);
-        console.log("Tag: " + selectedTag);
-        console.log("Year: " + selectedYear);
-        console.log("Season: " + selectedSeason);
-        console.log("Format: " + selectedFormat);
-        console.log("--------------------------")
-    }, [selectedName, selectedTag, selectedYear, selectedSeason, selectedFormat])
+        if (isAnimeLoading !== 'init') {
+            setIsAnimeLoading('done');
+        }
+    }, [searchResult])
 
+    //#region Render
+    if (isLoading === "loading") {
+        return <Loading />
+    }
+
+    let quote = getRandomQuote();
     return (
-        <div>
+        <div className="bg-dark-container" style={{ minHeight: "100vh" }}>
             <TextFilter
                 handleSelectName={handleSelectName}
                 tags={tags} handleSelectTag={handleSelectTag}
                 years={years} handleSelectYear={handleSelectYear}
                 seasons={seasons} handleSelectSeason={handleSelectSeason}
-                formats={formats} handleSelectFormat={handleSelectFormat} />
+                selectedName={selectedName} selectedYear={selectedYear} selectedSeason={selectedSeason} selectedFormat={selectedFormat}
+                formats={formats} handleSelectFormat={handleSelectFormat}>
+                <button className="input-group-text btn btn-info" id="basic-addon1" style={{ color: "white" }}
+                    onClick={handleSubmitSearch}
+                    ref={searchBtn}>
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </button>
+            </TextFilter>
             <SearchTagList selectedTag={selectedTag} handleDeleteTag={handleDeleteTag} />
-            {/* <SearchResult /> */}
+
+            { isAnimeLoading === 'init' ?
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <h4 className="pt-4 pb-2"><u>It's time to search anime!</u></h4>
+                    <img className="my-2 img-fluid" src={Cell} alt="Platelet - Hataraku Saibou" />
+                </div>
+                : isAnimeLoading === 'loading' ? 
+                    <div className="d-flex flex-column justify-content-center align-items-center py-3">
+                        <h4 className="text-light">{quote.quote}</h4>
+                        <p className="text-light"><i>---{quote.author}---</i></p>
+                        <div class="spinner-border text-light" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    : searchResult.length === 0 ?
+                        <div className="d-flex flex-column justify-content-center align-items-center" style={{ width: "100vw", height: "400px" }}>
+                            <h5 className="p-2"><u>Oops... We could not find your anime...</u></h5>
+                            <div className="my-2" style={KANNA_IMG_STYLE} ></div>
+                        </div>
+                        : <SearchResult searchResult={searchResult} />
+            }
         </div>
     );
+    //#endregion Render
 }
 
 export default SearchByText;
