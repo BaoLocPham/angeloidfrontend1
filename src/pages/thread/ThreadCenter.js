1e60876016659be00d144ee6a1b2c5e7c71d2d25
ViNguyenHoang <hoangvi0206@gmail.com>
2021-06-30 20:28:48 +0700

    Vi: code fontend search thread

--- src/pages/thread/ThreadCenter.js
+++ src/pages/thread/ThreadCenter.js
@@ -1,17 +1,100 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ThreadSearch from './ThreadSearch';
 import ThreadPosting from './ThreadPosting';
 import ThreadList from './ThreadList';
+import Kanna from '../error/Kanna.png';
+
+const KANNA_IMG_STYLE = {
+    height: "35rem",
+    width: "100%",
+    backgroundImage: `url(${Kanna})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover"
+}
 
 const ThreadCenter = ({ user, isLogin }) => {
     const [threadAdded, setThreadAdded] = useState(0);
+    const [threadList, setThreadList] = useState([
+        { threadId: 0 }
+    ]);
+    const [textInput, setTextInput] = useState("");
+    const [isSearch, setIsSearch] = useState(false);
+
+
+    function isEmptyOrSpaces(str) {
+        return str === null || str.match(/^ *$/) !== null;
+    }
+
+    // Handle when user search title
+    const handleSearchThread = (event) => {
+        event.preventDefault();
+        if (isEmptyOrSpaces(textInput)) {
+            setIsSearch(false);
+            fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/startup`,
+                {
+                    method: "GET",
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            )
+                .then(res => res.json())
+                .then(res => setThreadList(res))
+        } else {
+            setIsSearch(true);
+            fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/searchThread`, {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    searchString: textInput
+                })
+            })
+                .then(res => res.json())
+                .then(res => setThreadList(res))
+        }
+    }
+
+    //xóa vị trí cũ sau khi render lại 
+    useEffect(() => {
+        if (isSearch !== true) {
+            window.history.scrollRestoration = "manual";
+            fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/startup`,
+                {
+                    method: "GET",
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            )
+                .then(res => res.json())
+                .then(res => setThreadList(res))
+        }
+    }, []);
+    useEffect(() => {
+        if (isSearch !== true) {
+            fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/startup`,
+                {
+                    method: "GET",
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            )
+                .then(res => res.json())
+                .then(res => setThreadList(res))
+        }
+    }, [threadAdded]);
+
     return (
         <>
-            <ThreadSearch />
-            { isLogin ? 
+            <ThreadSearch handleSearchThread={handleSearchThread} setTextInput={setTextInput} />
+            {isLogin ?
                 <ThreadPosting user={user} threadAdded={threadAdded} setThreadAdded={setThreadAdded} />
-            : <h5 className="p-3">Please Login to post something!!!</h5> }
-            <ThreadList threadAdded = { threadAdded}/>
+                : <h5 className="p-3">Please Login to post something!!!</h5>}
+            {threadList.length === 0
+                ?
+                <div className="justify-content-center align-items-center">
+                    <h5 className="p-2"><u>Oops... We could not find your thread...</u></h5>
+                    <div className="my-2" style={KANNA_IMG_STYLE} ></div>
+                </div>
+                :
+                <ThreadList threadList={threadList} setThreadList={setThreadList} isSearch={isSearch} />
+            }
         </>
     );
 }
--- src/pages/thread/ThreadList.js
+++ src/pages/thread/ThreadList.js
@@ -2,38 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 import ThreadContent from './ThreadContent'
 
+const ThreadList = ({ threadList, setThreadList, isSearch }) => {
 
-
-const ThreadList = (threadAdded) => {
-    const [threadList, setThreadList] = useState([
-        {threadId:0}
-    ]);
     // last thread id -> for fetch api
     const [lastId, setLastId] = useState(0);
     // is there any thread left
     const [isAnyLeft, setIsAnyLeft] = useState(true);
-    //xóa vị trí cũ sau khi render lại 
-    useEffect(() => {
-        window.history.scrollRestoration = "manual";
-        fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/startup`,
-            {
-                method: "GET",
-                headers: { 'Content-Type': 'application/json' },
-            }
-        )
-            .then(res => res.json())
-            .then(res => setThreadList(res))
-    }, []);
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/startup`,
-            {
-                method: "GET",
-                headers: { 'Content-Type': 'application/json' },
-            }
-        )
-            .then(res => res.json())
-            .then(res => setThreadList(res))
-    }, [threadAdded]);
 
     function noScroll() {
         var x = window.scrollX;
@@ -42,37 +16,39 @@ const ThreadList = (threadAdded) => {
     }
 
     useEffect(() => {
-        const insidethreadList = document.getElementById("threadList");
-        window.addEventListener('scroll', () => {
-            //Nếu tọa độ hiện tại theo chiều dọc + chiều dài của màn hình người dùng
-            //Bằng với chiều dài của threadList cộng khoảng cách từ threadList hiện tại tới đầu trang - 16
-            //Thực hiện thâm dữ liệu 
-            if (isAnyLeft && window.scrollY + window.innerHeight > insidethreadList.clientHeight + insidethreadList.offsetTop - 16) {
-                // temporary disable scrolling
-                window.addEventListener('scroll', noScroll);
-                //Thực hiện cập nhật thêm Thread khi khéo xuống cuối trang
-                // setThreadList(prev => { return [...prev, ...updateThreadList] });
-                if (lastId != undefined) {
-                    fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/load/${lastId}`,
-                        {
-                            method: "GET"
-                        }
-                    )
-                        .then(res => res.json())
-                        .then(res => {
-                            if(res.length !== 0){
-                                setThreadList([...threadList, ...res]);
+        if (isSearch !== true) {
+            const insidethreadList = document.getElementById("threadList");
+            window.addEventListener('scroll', () => {
+                //Nếu tọa độ hiện tại theo chiều dọc + chiều dài của màn hình người dùng
+                //Bằng với chiều dài của threadList cộng khoảng cách từ threadList hiện tại tới đầu trang - 16
+                //Thực hiện thâm dữ liệu 
+                if (isAnyLeft && window.scrollY + window.innerHeight > insidethreadList.clientHeight + insidethreadList.offsetTop - 16) {
+                    // temporary disable scrolling
+                    window.addEventListener('scroll', noScroll);
+                    //Thực hiện cập nhật thêm Thread khi khéo xuống cuối trang
+                    // setThreadList(prev => { return [...prev, ...updateThreadList] });
+                    if (lastId != undefined) {
+                        fetch(`${process.env.REACT_APP_BACKEND_URL}api/thread/load/${lastId}`,
+                            {
+                                method: "GET"
                             }
-                            if(res.length == 0 ){ // if res is null=> there is no thread left
-                                setIsAnyLeft(false);
-                            }
-                        })
+                        )
+                            .then(res => res.json())
+                            .then(res => {
+                                if (res.length !== 0) {
+                                    setThreadList([...threadList, ...res]);
+                                }
+                                if (res.length == 0) { // if res is null=> there is no thread left
+                                    setIsAnyLeft(false);
+                                }
+                            })
+                    }
                 }
-            }
-        });
-        return () => {
-            window.removeEventListener('scroll', noScroll);
-        };
+            });
+            return () => {
+                window.removeEventListener('scroll', noScroll);
+            };
+        }
     });
 
     useEffect(() => {
@@ -84,9 +60,9 @@ const ThreadList = (threadAdded) => {
             {   // check if threadList is default or not 
                 // if threadlist is default then not load thread content
                 threadList.length === 1 ? "" :
-                threadList.map(thread => (
-                    <ThreadContent key={thread.threadId} content={thread} />
-                ))
+                    threadList.map(thread => (
+                        <ThreadContent key={thread.threadId} content={thread} />
+                    ))
             }
         </div>
     );
