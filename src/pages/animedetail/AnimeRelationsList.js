ac2ecf878817a99485e08c94d53d6da35090bc81
Guy Handsome <sakakiyuya1412@gmail.com>
2021-07-06 10:40:17 +0700

    Thinh: has changed Search Result show result

--- src/pages/animedetail/AnimeRelationsList.js
+++ src/pages/animedetail/AnimeRelationsList.js
@@ -3,27 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './AnimeRelation.css'
 
 const AnimeRelationsList = ({ anime }) => {
-    // const animeL = {
-    //     backgroundImage: `url(${ab})`,
-    //     backgroundSize: 'cover',    
-    //     width: '8rem',
-    //     height: '10rem',
-    //     borderRadius: '8px',
-    //     backgroundPosition: "center",
-    // }
+
     const frame = {
         backgroundColor: "#19293B",
         borderRadius: "8px",
         fontSize: "0.8rem",
     }
     return (
-        <div style={frame} className="my-2 anime-frame">
-            <div className="row">
-                <div className="col-2 relation" />
-                <div className="col-6">
-                    <div className="anime-title">{anime.name}</div>
-                    <br></br>
-                    <div className="anime-card">{anime.description}</div>
+        <div className=" relation col-12 col-md-6 mb-2 ">
+            <div style={frame} className="row mx-0">
+                <div className="row">
+                    <div className="col-2 relation" />
+                    <div className="col-6">
+                        <div className="anime-title">{anime.name}</div>
+                        <br></br>
+                        <div className="anime-card">{anime.description}</div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/search/SearchResult.js
+++ src/pages/search/SearchResult.js
@@ -4,41 +4,29 @@ import './SearchResult.css'
 
 
 const SearchResult = ({ searchResult }) => {
-    // const [animeList, setAnimeList] = useState([
-    //     { animeId: 1, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", Tag: [{ tagId: "rape", tagName: "abc"}] }
-    // ]);
-
-    // const animeList = [
-    //     { animeId: 1, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 2, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 3, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 4, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 5, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 6, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 7, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 8, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 8, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] },
-    //     { animeId: 10, animeName: 'Konosuba', thumbnail: "", studio: { studioName: "Hello" }, format: "TV series", episode: "25 episodes", tags: [{ tagId: "rape", tagName: "abc" }] }
-    // ];
 
     if (searchResult.length === 0) {
         return null;
     }
 
-    return (
-        <div className="w-100 h-auto" style={{ backgroundColor: "#131E2A", color: "#fff", margin: "auto ", width: "100%", padding: "0 5% 0 5%", minHeight: "74vh" }}>
-            <div className="w-100 h-auto row mx-0 justify-content-start search-result" style={{ textAlign: "center" }}>
-                {
-                    searchResult.map(
-                        anime => (
-                            <div className="col-2 p-2">
-                                <AnimeCard key={anime.animeId} anime={anime} isVertical={false} />
-                            </div>
+    var count = -1;
 
+    return (
+        <div className="w-100 mx-2 py-4" style={{ backgroundColor: "#131E2A", color: "#fff", margin: "auto ",  padding: "0 5% 0 5%", minHeight: "74vh" }}>
+            <div>
+                    <h5 style={{ display: "inline-block", width: "50%" }}>SEARCH RESULTS</h5>
+                </div>
+                <div className="w-100 h-auto row justify-content-start" style={{ textAlign: "center" }}>
+                    {
+                        searchResult.map(
+                            anime => (
+                                //increase count when load over an anime
+                                count++,
+                                <AnimeCard key={anime.animeId} anime={anime} isVertical={false} count={count} />
+                            )
                         )
-                    )
-                }
-            </div>
+                    }
+                </div>
         </div>
     );
 }
